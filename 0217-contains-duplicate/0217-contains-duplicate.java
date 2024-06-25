bfe0cfd6a0896ea99b54a92a088d87ce4144c7ca
Shubham Raskar <shubhamraskar169@gmail.com>
2024-06-25 12:57:52 +0530

    Time: 19 ms (26.43%), Space: 54.5 MB (99.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,24 +1,12 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        // for(int i=0; i<nums.length; i++)
-        // {
-        //     for(int j=i+1; j<nums.length; j++)
-        //     {
-        //         if(nums[i] == nums[j]){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return false;
-        
-        Arrays.sort(nums);
-        
-        for(int i=0; i<nums.length-1; i++)
-        {
-            if(nums[i] == nums[i+1]){
-                return true;
-            }
+public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+    for(int i=0; i<nums.length-1; i++)
+    {
+        if(nums[i] == nums[i+1]){
+            return true;
         }
-        return false;
     }
+    return false;
+}
 }
