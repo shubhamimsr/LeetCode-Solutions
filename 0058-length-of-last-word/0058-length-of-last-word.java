c4806f0cb39a24d95f0768cd4c701e7f753811fb
Shubham Raskar <shubhamraskar169@gmail.com>
2024-08-18 18:34:18 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (30.41%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String [] str = s.split(" ");
-        String lastWord = lastWord = str[str.length-1];
+        String lastWord = str[str.length-1];
         return lastWord.length();
     }
 }
