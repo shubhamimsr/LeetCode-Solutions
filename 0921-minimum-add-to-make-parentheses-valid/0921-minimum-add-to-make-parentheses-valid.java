f81ac37971141ab1704362d8b0c40aca0f82b16e
Shubham Raskar <shubhamraskar169@gmail.com>
2024-10-09 10:45:46 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (28.39%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int openBrackets = 0, minAddsRequired = 0;
+        
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                openBrackets++;
+            }else{
+                if(openBrackets > 0){
+                    openBrackets--;
+                }else{
+                    minAddsRequired++;
+                }
+            }
+        }
+        return minAddsRequired + openBrackets;
+    }
+}
