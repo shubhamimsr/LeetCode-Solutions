67bf11edd145c444e26fe488d644ff487caacdc9
Shubham Raskar <shubhamraskar169@gmail.com>
2024-11-12 19:53:56 +0530

    Time: 24 ms (31.91%), Space: 46.6 MB (97.32%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        TreeMap<Integer, Integer> startPoints = new TreeMap<>();
+        for(int i=0; i<intervals.length; i++){
+            startPoints.put(intervals[i][0], i);
+        }
+        
+        int[] ans = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++){
+            Integer nextClosestStartPoint = startPoints.ceilingKey(intervals[i][1]);
+            ans[i]= nextClosestStartPoint==null ? -1 : startPoints.get(nextClosestStartPoint);
+        }
+        return ans;
+    }
+}
