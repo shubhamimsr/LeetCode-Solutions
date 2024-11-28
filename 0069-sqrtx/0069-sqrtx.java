773b59314e6f9ac747caf240a54e1710ac94c8b6
Shubham Raskar <shubhamraskar169@gmail.com>
2024-11-28 21:08:19 +0530

    Time: 1 ms (86.70%), Space: 41.1 MB (22.97%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,9 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);   
+        // return (int)Math.sqrt(x);   
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;
+        return (int) r;
     }
 }
