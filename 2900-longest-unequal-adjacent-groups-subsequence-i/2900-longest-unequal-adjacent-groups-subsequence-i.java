02f2180f1aa201ec987494516078c922f956d0d5
Shubham Raskar <shubhamraskar169@gmail.com>
2025-05-15 23:10:55 +0530

    Time: 1 ms (96.15%), Space: 45.3 MB (15.18%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.java
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.java
@@ -1,21 +1,35 @@
+// class Solution {
+//     public List<String> getLongestSubsequence(String[] words, int[] groups) {
+
+//         int left = groups[0];
+//         List<String> result = new ArrayList<>();
+
+//         if (groups.length == 1) {
+//             result.add(words[0]);
+//             return result;
+//         }
+
+//         result.add(words[0]);
+//         for (int i = 1; i < groups.length; i++) {
+//             if (groups[i] != left) {
+//                 left = groups[i];
+//                 result.add(words[i]);
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
-        
-        int left = groups[0];
-        List<String> result = new ArrayList<>();
-       
-        if(groups.length == 1){
-            result.add(words[0]);
-            return result;
-        }
-        
-         result.add(words[0]);
-        for(int i=1; i<groups.length; i++){
-            if(groups[i] != left){
-                left = groups[i];
-                result.add(words[i]);
+        List<String> res = new ArrayList<>();
+        res.add(words[0]);
+        for (int i = 1; i < groups.length; ++i) {
+            if (groups[i] != groups[i - 1]) {
+                res.add(words[i]);
             }
         }
-        return result;
+        return res;
     }
 }
