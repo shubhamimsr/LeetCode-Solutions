ac8b59e3858401200755548f4a700d2fa90b3654
Shubham Raskar <shubhamraskar169@gmail.com>
2025-05-19 20:30:23 +0530

    Time: 0 ms (100%), Space: 41.9 MB (74.91%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.java
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -1,5 +1,28 @@
 class Solution {
     public String triangleType(int[] nums) {
+        // No need to assign to separate variables; use array indices directly
+
+        if (isTriangle(nums)) {
+            if (nums[0] == nums[1] && nums[1] == nums[2]) {
+                return "equilateral";
+            } else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[2] == nums[0]) {
+                return "isosceles";
+            } else {
+                return "scalene";
+            }
+        }
+        return "none";
+    }
+
+    private boolean isTriangle(int[] nums) {
+        return nums[0] + nums[1] > nums[2] &&
+               nums[1] + nums[2] > nums[0] &&
+               nums[2] + nums[0] > nums[1];
+    }
+}
+
+/*class Solution {
+    public String triangleType(int[] nums) {
         // Arrays.sort(nums);
         int first = nums[0];
         int second = nums[1];
@@ -18,13 +41,13 @@ class Solution {
         }
     }
 
-    public boolean isTriangle(int [] nums){
+    public boolean isTriangle(int[] nums) {
         int first = nums[0];
         int second = nums[1];
         int third = nums[2];
-        if(first+second>third && second+third>first && first+third>second)
+        if (first + second > third && second + third > first && first + third > second)
             return true;
         else
             return false;
     }
-}
+}*/
