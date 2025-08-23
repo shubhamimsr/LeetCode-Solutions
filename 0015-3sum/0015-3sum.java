1cfa2c6539714c29a2c69536518311bea1c4d25e
Shubham Raskar <shubhamraskar169@gmail.com>
2025-08-24 00:01:45 +0530

    Time: 830 ms (14.4%), Space: 52.3 MB (25.68%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> resultList = new ArrayList<>();
+        Set<List<Integer>> uniqueList = new HashSet<>();
         Arrays.sort(nums);
-        
-        for(int i=0; i<nums.length-1; i++){
-            int left = i+1;
-            int right = nums.length-1;
-            while(left<right){
-                if(nums[i]+nums[left]+nums[right]<0){
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] < 0) {
                     left++;
-                }else if(nums[i]+nums[left]+nums[right]>0){
+                } else if (nums[i] + nums[left] + nums[right] > 0) {
                     right--;
-                }else{
-                    set.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                }else {
+                    uniqueList.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
                 }
             }
         }
-        result.addAll(set);
-        return result;
+        resultList.addAll(uniqueList);
+        return resultList;
+
     }
 }
