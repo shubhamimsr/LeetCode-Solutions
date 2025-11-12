d9680287df73ccd1beadef3c492292dcb815f39a
Shubham Raskar <shubhamraskar169@gmail.com>
2025-11-12 19:59:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,32 +1,9 @@
--- Write your PostgreSQL query statement below
--- select (select DISTINCT salary 
--- from Employee
--- order by salary desc
--- offset 1 
--- limit 1) as SecondHighestSalary;
-
--- SELECT MAX(salary) AS SecondHighestSalary
--- FROM Employee
--- WHERE salary < (SELECT MAX(salary) FROM Employee);
-
--- select max(salary) as SecondHighestSalary
--- from employee
--- where salary < (select max(salary) from employee);
-
--- SELECT distinct salary as SecondHighestSalary
--- from (
---     select id, salary, RANK() 
---     OVER (ORDER BY salary DESC) AS salary_rank
---     FROM Employee
--- )ranked
--- where salary_rank = 2;
-
--- Write your PostgreSQL query statement below
-WITH r AS (SELECT DISTINCT salary FROM Employee)
-SELECT (
-    SELECT salary  
-    FROM (
-        SELECT salary, rank() OVER (ORDER BY salary DESC) rank FROM r
-    )
-    WHERE rank = 2
-)AS "SecondHighestSalary";
+-- -- Write your PostgreSQL query statement below
+ SELECT MAX(salary) as SecondHighestSalary
+ FROM(   
+    SELECT salary,
+    rank() over(order by salary DESC) as ranked_position
+    FROM Employee
+ ) as ranked
+ 
+ WHERE ranked_position = 2;
