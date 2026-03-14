4677191a934af01d230448e64231bfa2e69ded0b
Shubham Raskar <shubhamraskar169@gmail.com>
2026-03-14 23:12:17 +0530

    Time: 5 ms (84.24%), Space: 47.2 MB (24.73%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,47 +1,102 @@
 class MinStack {
-
-    private Stack<Integer> stack;
-    private Stack<Integer> ss;
+    private Stack<Long> stack;
+    long minElement;
 
     public MinStack() {
         this.stack = new Stack<>();
-        this.ss = new Stack<>();
     }
 
     public void push(int val) {
-        stack.push(val);
-        if(ss.isEmpty() || val <= ss.peek()){
-            ss.push(val);
+        if (stack.isEmpty()) {
+            stack.push((long)val);
+            minElement = val;
+        } else {
+            if (val >= minElement) {
+                stack.push((long)val);
+            } else if (val < minElement) {
+                stack.push(2L * val - minElement);
+                minElement=val;
+            }
         }
-        return;
     }
 
     public void pop() {
         if(stack.isEmpty()){
             return;
+        }else{
+            if(stack.peek() >= minElement){
+                stack.pop();
+            }else if(stack.peek() < minElement){
+                minElement = 2L*minElement - stack.peek();
+                stack.pop();
+            }
         }
-        int ans = stack.peek();
-        stack.pop();
-        if(ss.peek() == ans){
-            ss.pop();
-        }
-        return;
     }
 
     public int top() {
-        if(stack.isEmpty()){
+        long top = stack.peek();
+        if (stack.isEmpty()) {
             return -1;
+        } else {
+            if (top >= minElement) {
+                return (int)top;
+            } else if (top < minElement) {
+                return (int)minElement;
+            }
         }
-        return stack.peek();
+        return -1;
     }
 
     public int getMin() {
-        if (ss.isEmpty()) {
+        if(stack.isEmpty())
             return -1;
-        }
-        return ss.peek();
+        return (int)minElement;
     }
 }
+// class MinStack {
+
+//     private Stack<Integer> stack;
+//     private Stack<Integer> ss;
+
+//     public MinStack() {
+//         this.stack = new Stack<>();
+//         this.ss = new Stack<>();
+//     }
+
+//     public void push(int val) {
+//         stack.push(val);
+//         if(ss.isEmpty() || val <= ss.peek()){
+//             ss.push(val);
+//         }
+//         return;
+//     }
+
+//     public void pop() {
+//         if(stack.isEmpty()){
+//             return;
+//         }
+//         int ans = stack.peek();
+//         stack.pop();
+//         if(ss.peek() == ans){
+//             ss.pop();
+//         }
+//         return;
+//     }
+
+//     public int top() {
+//         if(stack.isEmpty()){
+//             return -1;
+//         }
+//         return stack.peek();
+//     }
+
+//     public int getMin() {
+//         if (ss.isEmpty()) {
+//             return -1;
+//         }
+//         return ss.peek();
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
