39938075bceb2a53f94437a21f85b60c522506fd
Shubham Raskar <shubhamraskar169@gmail.com>
2026-03-19 23:39:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,30 +1,53 @@
 class Solution {
     public List<Integer> findClosestElements(int[] nums, int k, int x) {
-        //using MAX_HEAP
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-                (a, b) -> {
-                    if (a[0] != b[0]) {
-                        return Integer.compare(b[0], a[0]);
-                    } else {
-                        return Integer.compare(b[1], a[1]);
-                    }
-                });
-        int n = nums.length;
+        int left=0;
+        int right=nums.length-1;
 
-        for (int i = 0; i < n; i++) {
-            int temp = Math.abs(nums[i] - x);
-            pq.add(new int[] { temp, nums[i] });
-            if (pq.size() > k) {
-                pq.poll();
+        while(right-left + 1 > k){
+            int l=Math.abs(nums[left]-x);
+            int r = Math.abs(nums[right]-x);
+            if(l>r){
+                left++;
+            }else{
+                right--;
             }
         }
-
         List<Integer> list = new ArrayList<>();
-        while (!pq.isEmpty()) {
-            list.add(pq.poll()[1]);
-
+        for(int i=left; i<=right; i++){
+            list.add(nums[i]);
         }
-        Collections.sort(list);
         return list;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> findClosestElements(int[] nums, int k, int x) {
+//         //using MAX_HEAP
+//         PriorityQueue<int[]> pq = new PriorityQueue<>(
+//                 (a, b) -> {
+//                     if (a[0] != b[0]) {
+//                         return Integer.compare(b[0], a[0]);
+//                     } else {
+//                         return Integer.compare(b[1], a[1]);
+//                     }
+//                 });
+//         int n = nums.length;
+
+//         for (int i = 0; i < n; i++) {
+//             int temp = Math.abs(nums[i] - x);
+//             pq.add(new int[] { temp, nums[i] });
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+
+//         List<Integer> list = new ArrayList<>();
+//         while (!pq.isEmpty()) {
+//             list.add(pq.poll()[1]);
+
+//         }
+//         Collections.sort(list);
+//         return list;
+//     }
+// }
