a9e55224877b427c20ba34c2eb84d13e9d3f7bbe
Shubham Raskar <shubhamraskar169@gmail.com>
2026-03-20 20:34:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -3,13 +3,12 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num,0)+1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-            pq.add(new int[] { (int)entry.getValue(), (int)entry.getKey() });
+            pq.add(new int[] { (int) entry.getValue(), (int) entry.getKey() });
             if (pq.size() > k) {
                 pq.poll();
             }
