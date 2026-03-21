b5dfdf34f4e58fc3b9343742010758b7bcad1c72
Shubham Raskar <shubhamraskar169@gmail.com>
2026-03-21 22:06:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -6,32 +6,27 @@ class Solution {
         }
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b)->{
-                if(a[0] != b[0]){
-                    return a[0]-b[0];
-                }
-                return b[1]-a[1];
-            }
-        );
+                (a, b) -> {
+                    if (a[0] != b[0]) {
+                        return a[0] - b[0];
+                    }
+                    return b[1] - a[1];
+                });
 
         for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            // System.out.println(e.getKey()+" "+e.getValue());
             pq.add(new int[] { (int) e.getValue(), (int) e.getKey() });
         }
-        // boolean hasDupeValues = new HashSet<>(map.values()).size() != map.size();
-        // System.out.println(hasDupeValues);
+
         int[] result = new int[nums.length];
         int idx = 0;
         while (!pq.isEmpty()) {
             int top[] = pq.poll();
-
             int freq = top[0];
             int num = top[1];
             for (int i = 0; i < freq; i++) {
                 result[idx++] = num;
             }
         }
-
         return result;
     }
 }
