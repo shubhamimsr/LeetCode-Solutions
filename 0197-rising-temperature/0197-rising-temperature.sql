90d511bacf55ec87c9d3b913631d725a5efa2a94
Shubham Raskar <shubhamraskar169@gmail.com>
2026-04-15 23:46:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,6 @@
 -- Write your PostgreSQL query statement below
-select w.id
-from Weather w
-where lead() over (partition by w.recordDate)
+SELECT w2.id
+FROM Weather w1
+JOIN Weather w2
+  ON w2.recordDate = w1.recordDate + 1
+WHERE w2.temperature > w1.temperature;
