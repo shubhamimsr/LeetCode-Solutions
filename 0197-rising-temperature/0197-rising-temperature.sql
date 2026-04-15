757171bd7f9e54aa0019406229d1154f9cd2d8d0
Shubham Raskar <shubhamraskar169@gmail.com>
2026-04-15 23:43:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-SELECT w2.id
-FROM Weather AS w1
-join Weather AS w2
-WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1 
-AND w2.temperature > w1.temperature;
+-- Write your PostgreSQL query statement below
+select w.id
+from Weather w
+where lead() over (partition by w.recordDate)
