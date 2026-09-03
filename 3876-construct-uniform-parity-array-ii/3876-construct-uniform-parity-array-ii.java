0cc6d8bdcf832a84ee91abe81aee5cfae1d989d4
Shubham Raskar <shubhamraskar169@gmail.com>
2026-09-03 12:08:23 +0530

    Time: 9 ms (34.78%), Space: 119.5 MB (90.58%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int n = nums1.length;
+
+        if (checkAllOdds(nums1) || checkAllEvens(nums1)) {
+            return true;
+        }
+
+        int small = getSmallest(nums1);
+        boolean isSmallEven = small % 2 == 0 ? true : false;
+
+        int[] nums2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (isSmallEven && nums1[i] % 2 != 0) {
+                nums2[i] = nums1[i] - small;
+            } else if (!isSmallEven && nums1[i] % 2 == 0) {
+                nums2[i] = nums1[i] - small;
+            } else {
+                nums2[i] = nums1[i];
+            }
+        }
+        if (checkAllOdds(nums2) || checkAllEvens(nums2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int getSmallest(int arr[]) {
+        int small = arr[0];
+        for (int n : arr) {
+            small = Math.min(small, n);
+        }
+        return small;
+    }
+
+    private static boolean checkAllOdds(int arr[]) {
+        for (int n : arr) {
+            if (n % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkAllEvens(int arr[]) {
+        for (int n : arr) {
+            if (n % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
